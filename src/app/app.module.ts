32c29baac6ad005f4c30f976c137f4e30339a45c
allania7med11 <allania7med11@gmail.com>
2021-02-28 13:04:09 +0100

    icon create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
