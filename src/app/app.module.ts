67a81f012cc9a7f6b939248cad9105a63546a3cc
allania7med11 <allania7med11@gmail.com>
2021-02-28 20:26:03 +0100

    icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { IconComponent } from './icon/icon.component';
+import { MailComponent } from './icons/mail.component';
+import { LockClosedComponent } from './icons/lock-closed.component';
+import { UserComponent } from './icons/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    IconComponent
+    IconComponent,
+    MailComponent,
+    LockClosedComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  fields = [
+    { type: 'text', name: 'username', placeholder: 'Username', icon: 'user' },
+    { type: 'text', name: 'mail', placeholder: 'Email', icon: 'mail' },
+    {
+      type: 'password',
+      name: 'password',
+      placeholder: 'Password',
+      icon: 'lock-closed',
+    },
+    {
+      type: 'password',
+      name: 'password2',
+      placeholder: 'Password Confirmation',
+      icon: 'lock-closed',
+    },
+  ];
+  ngOnInit(): void {}
 }
